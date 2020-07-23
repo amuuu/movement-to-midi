b8bc7ef32f66a9b27ada8a04f1d59ccae91fa8ef
Amirreza Amouie <aamouie01@gmail.com>
2020-07-23 12:32:49 -0700

    read one sensor's data

--- ardunio/main/main.ino
+++ ardunio/main/main.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
 
+const int NUM_PIR_SENSORS = 1;
+int pir_pins[] = {0}; // Analog pins which are connected to the PIR sensors
+bool pir_triggers[] = {false};
+int pir_reads[] = {-1};
+
+void setup() {
+  Serial.begin(9600);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+void readPirSensors(){
+  for (int i=0; i<NUM_PIR_SENSORS; i++){
+    pir_reads[i] = analogRead(pir_pins[i]);
+    Serial.println(String(pir_reads[i]));
+  }
 }
